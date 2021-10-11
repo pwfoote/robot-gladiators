8dd8839f1ba58bf806a958604058746b55d95a44
Preston Foote <prestonwfoote@gmail.com>
2021-10-11 17:57:03 -0400

    created fight function, player if/else, enemy if/else

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,47 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName)
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
-function fight() {
-    window.alert("The fight has begun!");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 100;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+        );
+
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died! ");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+        }
+playerHealth = playerHealth - enemyAttack;
+
+    console.log(
+        enemyName + " attacked " + playerName +". " + playerName + " now has " + playerHealth + " health remaining. "
+    );
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " still has " + playerHealth + " health left. ");
+    }
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+  // Log a resulting message to the console so we know that it worked.
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+  // Log a resulting message to the console so we know that it worked.
+
 }
-// fight();
+
+fight();
+
 
